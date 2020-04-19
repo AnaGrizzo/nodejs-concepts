d2b6404181a5abc96a0b0c289a48cb156e057625
Ana Grizzo <almamg91@gmail.com>
2020-04-19 14:44:48 -0300

    app.post in progress

--- src/app.js
+++ src/app.js
@@ -14,6 +14,18 @@ app.get("/repositories", (request, response) => {
   // TODO
 });
 
+/**
+ * A rota deve receber title, url e techs dentro do corpo da requisição, sendo a 
+ * URL o link para o github desse repositório. Ao cadastrar um novo projeto, ele 
+ * deve ser armazenado dentro de um objeto no seguinte formato: 
+ * { id: "uuid", 
+ * title: 'Desafio Node.js', 
+ * url: 'http://github.com/...', 
+ * techs: ["Node.js", "..."], 
+ * likes: 0 }; 
+ * Certifique-se que o ID seja um UUID, e de sempre iniciar os likes como 0.
+ */
+
 app.post("/repositories", (request, response) => {
   // TODO
 });
